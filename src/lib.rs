701ea9d988b26b76b4ab83fa47d4c5fed934669d
WLBF <lbf1353@live.com>
2017-10-05 18:40:29 +0800

    change implementation of iswow64

--- src/lib.rs
+++ src/lib.rs
@@ -17,48 +17,26 @@
 //!
 //! ```
 
-#[macro_use]
-extern crate cpp;
+#[cfg(windows)]
+extern crate winapi;
+#[cfg(windows)]
+extern crate kernel32;
 
-cpp!{{
-    #include <windows.h>
-    #include <tchar.h>
-}}
+use winapi::BOOL;
+use kernel32::{IsWow64Process, GetCurrentProcess, GetLastError};
 
-pub fn iswow64() -> Result<bool, &'static str>{
+#[cfg(windows)]
+pub fn iswow64() -> Result<bool, u32>{
+    let mut is_wow64: BOOL = 0;
     unsafe {
-        let result: i32 = cpp!([]  -> i32 as "int32_t" {
-            typedef BOOL (WINAPI *LPFN_ISWOW64PROCESS) (HANDLE, PBOOL);
-
-            LPFN_ISWOW64PROCESS fnIsWow64Process;
-
-            BOOL bIsWow64 = FALSE;
-
-            //IsWow64Process is not available on all supported versions of Windows.
-            //Use GetModuleHandle to get a handle to the DLL that contains the function
-            //and GetProcAddress to get a pointer to the function if available.
-
-            fnIsWow64Process = (LPFN_ISWOW64PROCESS) GetProcAddress(
-                GetModuleHandle(TEXT("kernel32")),"IsWow64Process");
-
-            if(NULL == fnIsWow64Process)
-                return -1;
-            if (!fnIsWow64Process(GetCurrentProcess(),&bIsWow64))
-                return -2;
-            return bIsWow64;
-        });
-
-        match result {
-            0 => Ok(false),
-            1 => Ok(true),
-            -1 => Err("get IsWow64Process function failed!"),
-            -2 => Err("calling IsWow64Process function failed!"),
-            _ => unreachable!(),
+        match IsWow64Process(GetCurrentProcess(), &mut is_wow64) {
+            0 => Err(GetLastError()),
+            _ => Ok(is_wow64 == 1),
         }
     }
 }
 
-#[cfg(test)]
+#[cfg(all(test, windows))]
 mod test {
     use super::*;
 
